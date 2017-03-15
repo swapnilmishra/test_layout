9a03517a4e9c98d664d3b46d4c59c055004e427d
Swapnil Mishra <swapmis1084@gmail.com>
2017-03-15 15:02:07 +0530

    Adding doc and proptypes for Filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,3 +1,21 @@
+/**
+ * This is a filter component which can be used any where to populate set of filters which checkbox. Required parameters are given below via proptypes validation
+ *
+ Filter.propTypes = {
+  data : React.PropTypes.any.isRequired,
+  addFilter : React.PropTypes.any.isRequired,
+  removeFilter : React.PropTypes.any.isRequired,
+  filterValues: function(props, propName, componentName) {
+    if (!props.data.values) {
+      return new Error(
+        `This component requires value to be passed into data object`
+      );
+    }
+  }
+}
+ *  
+ */
+
 import React from "react";
 
 class Filter extends React.Component {
@@ -31,4 +49,19 @@ class Filter extends React.Component {
     }
   }
 }
+
+Filter.propTypes = {
+  data : React.PropTypes.any.isRequired,
+  addFilter : React.PropTypes.any.isRequired,
+  removeFilter : React.PropTypes.any.isRequired,
+  filterValues: function(props, propName, componentName) {
+    if (!props.data.values) {
+      return new Error(
+        `This component requires value to be passed into data object`
+      );
+    }
+  }
+}
+
+
 export default Filter;
