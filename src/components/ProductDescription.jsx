b725bdcfdb43d84c8debf29281d18199268153ad
Swapnil Mishra <swapmis1084@gmail.com>
2017-03-15 15:01:19 +0530

    finishing up product description page

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -1,3 +1,6 @@
+/**
+ * Product description Component which takes data and show various details for the product
+ */
 import React from "react";
 
 class ProductDesc extends React.Component {
@@ -8,10 +11,19 @@ class ProductDesc extends React.Component {
     const {productDetails} = this.props
     const imagePath = `img/${productDetails.image}`
     return (
-      <div>
-        <div>{productDetails.name}</div>
-        <img src={imagePath} alt="{productDetails.name}"/>
-        <button onClick={this.handleAddToCart}> Add to cart</button>
+      <div className='product-details-container-outer'>
+        <h1>{productDetails.name}</h1>
+        <div className='product-details-container'>
+          <div>
+            <img src={imagePath} alt={productDetails.name} className="item-img"/>
+          </div>
+          <div className="product-details">
+            <h3>{productDetails.measurement}</h3>
+            <h2>${productDetails.price}</h2>
+            <div className='product-details-summary'>{productDetails.desc}</div>
+            <button onClick={this.handleAddToCart} className='add-to-cart button'> Add to cart</button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -20,4 +32,9 @@ class ProductDesc extends React.Component {
     this.props.cb(this.props.productDetails)
   }
 }
+
+ProductDesc.propTypes = {
+  productDetails : React.PropTypes.any.isRequired && React.PropTypes.object
+}
+
 export default ProductDesc;
